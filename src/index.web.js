937951ca06b57799ebdc778a259d318e72fec90e
Leonardo Morini <leo.morini.nn@gmail.com>
2021-09-16 12:33:28 -0300

    [ master ] - organization

---
+++ src/index.web.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Widget from './Widget';
+
+function widgetInit() {
+  const WidgetDivs = document.querySelectorAll('.rnw-widget');
+
+  // Inject our React App into each
+  WidgetDivs.forEach(Div => {
+    console.log(Div);
+
+    ReactDOM.render(
+      <React.StrictMode>
+        <Widget />
+      </React.StrictMode>,
+      Div
+    );
+  });
+}
